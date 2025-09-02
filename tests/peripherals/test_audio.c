699e6d41357137540b3dc5ee4474073537eff9bb
Will Hedgecock <will.hedgecock@gmail.com>
2025-09-02 14:55:17 -0500

    Update audio test

--- tests/peripherals/test_audio.c
+++ tests/peripherals/test_audio.c
@@ -8,22 +8,23 @@
 // Change this definition to either constantly trigger audio reads or wait for a certain loudness
 #define AUDIO_MIC_TYPE                          MIC_DIGITAL
 #define AUDIO_READ_TRIGGER                      TRIGGER_IMMEDIATE
-#define AUDIO_GAIN_DB                           35.0        // Max of 35.0
+#define AUDIO_SAMPLING_RATE                     AUDIO_DEFAULT_SAMPLING_RATE_HZ
+#define AUDIO_GAIN_DB                           25.0        // Max of 35.0
 #define AUDIO_TRIGGER_THRESHOLD_PERCENT         0.5
-#define AUDIO_CLIP_LENGTH_SECONDS               10
+#define AUDIO_CLIP_LENGTH_SECONDS               AUDIO_DEFAULT_CLIP_LENGTH_SECONDS
 
 int main(void)
 {
    // Set up the system hardware
    setup_hardware();
    if (AUDIO_MIC_TYPE == MIC_ANALOG)
-      audio_analog_init(AUDIO_NUM_CHANNELS, AUDIO_DEFAULT_SAMPLING_RATE_HZ, AUDIO_GAIN_DB, AUDIO_MIC_BIAS_VOLTAGE, AUDIO_READ_TRIGGER, AUDIO_TRIGGER_THRESHOLD_PERCENT);
+      audio_analog_init(AUDIO_NUM_CHANNELS, AUDIO_SAMPLING_RATE, AUDIO_GAIN_DB, AUDIO_MIC_BIAS_VOLTAGE, AUDIO_READ_TRIGGER, AUDIO_TRIGGER_THRESHOLD_PERCENT);
    else
-      audio_digital_init(AUDIO_NUM_CHANNELS, AUDIO_DEFAULT_SAMPLING_RATE_HZ, AUDIO_GAIN_DB);
+      audio_digital_init(AUDIO_NUM_CHANNELS, AUDIO_SAMPLING_RATE, AUDIO_GAIN_DB);
    system_enable_interrupts(true);
 
    // Loop forever handling incoming audio clips
-   static int16_t audio_buffer[AUDIO_BUFFER_NUM_SAMPLES];
+   static int16_t audio_buffer[AUDIO_SAMPLING_RATE];
    bool audio_clip_in_progress = false;
    uint32_t num_audio_reads = 0;
    while (true)
@@ -31,7 +32,7 @@ int main(void)
       // Determine if time to start listening for a new audio clip
       if (!audio_clip_in_progress)
       {
-         print("Initializing audio read trigger\n");
+         printonly("Initializing audio read trigger\n");
          audio_begin_reading();
          audio_clip_in_progress = true;
       }
@@ -45,11 +46,11 @@ int main(void)
       // Handle any newly available audio data
       if (audio_data_available() && audio_read_data(audio_buffer))
       {
-         print("New audio data available: %u\n", num_audio_reads+1);
+         printonly("New audio data available: %u\n", num_audio_reads+1);
          transmit_audio(audio_buffer, sizeof(audio_buffer));
          if (AUDIO_CLIP_LENGTH_SECONDS && (++num_audio_reads >= AUDIO_CLIP_LENGTH_SECONDS))
          {
-            print("Full audio clip processed...stopping reading\n");
+            printonly("Full audio clip processed...stopping reading\n");
             audio_clip_in_progress = false;
             audio_stop_reading();
             num_audio_reads = 0;
